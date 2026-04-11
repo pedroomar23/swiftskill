9cce3a81722ad23fdf4fe9f7083f3e2fd82ee781
Pedro Omar <pocl9812@gmail.com>
2026-04-11 11:15:18 -0400

    add: first commit

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftSkill",
-            targets: ["SwiftSkill"]
+            targets: ["SwiftSkill"] 
         ),
     ],
     targets: [
